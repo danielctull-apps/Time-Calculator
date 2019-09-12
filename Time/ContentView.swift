6d4880fc44870403a4072e1724c00b0162420a7f
Daniel Tull <dt@danieltull.co.uk>
2019-09-12 16:46:37 +0100

    Initial Commit

---
+++ Time/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time
+//
+//  Created by Daniel Tull on 12/09/2019.
+//  Copyright © 2019 Daniel Tull. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
